d52a138034a9055d4171bad1fb752239da2ffe13
ANA3501 <pv27649@alunos.estgv.ipv.pt>
2026-07-01 11:38:24 +0100

    done

--- frontend/src/Home.jsx
+++ frontend/src/Home.jsx
@@ -5,7 +5,7 @@ import ProdutosDestaque from './components/ProdutosDestaque';
 import StoryBanner from './components/StoryBanner';
 import Novidades from './components/Novidades';
 import Tutoriais from './components/Tutoriais';
-import Footer from './components/Footer';
+import Footer from './components/footer';
 
 function HomePage() {
   return (
